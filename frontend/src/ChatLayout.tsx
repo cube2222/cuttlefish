c6fc27faf44efa9967389d250152ff9438d7282c
Jakub Martin <jakub.wit.martin@gmail.com>
2023-04-02 14:49:55 +0200

    Continued.

--- frontend/src/ChatLayout.tsx
+++ frontend/src/ChatLayout.tsx
@@ -54,7 +54,12 @@ const ChatLayout = () => {
                 components={{
                     code({ node, inline, className, children, ...props }) {
                         const match = /language-(\w+)/.exec(className || '')
-                        return !inline && match ? (
+                        if (inline) {
+                            return <code className={className} {...props}>
+                                {children}
+                            </code>
+                        }
+                        return match ? (
                             <SyntaxHighlighter
                                 children={String(children).replace(/\n$/, '')}
                                 style={dracula as any}
