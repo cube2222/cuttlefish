2688f28468f9e0313a7a5afd6d54f928d4166e16
Jakub Martin <jakub.wit.martin@gmail.com>
2023-04-06 22:07:00 +0200

    Continued.

--- frontend/src/Sidebar.tsx
+++ frontend/src/Sidebar.tsx
@@ -1,48 +1,50 @@
-import {Settings} from "iconoir-react";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {database} from "../wailsjs/go/models";
-import Conversation = database.Conversation;
 import SettingsButton from "./SettingsButton";
+import {Conversations} from "../wailsjs/go/main/App";
+import {EventsOn} from "../wailsjs/runtime";
+import Conversation = database.Conversation;
 
 interface Props {
 }
 
 const Sidebar = ({}: Props) => {
-    const conversations = [
-        {
-            id: 1,
-            message: "Hey, how's it going?",
-            timestamp: "10:23 AM",
-        },
-        {
-            id: 2,
-            message: "Can you send me the report?",
-            timestamp: "Yesterday",
-        },
-        {
-            id: 3,
-            message: "I'll be late to the meeting",
-            timestamp: "Tuesday",
-        },
-    ];
+    const [conversations, setConversations] = useState<Array<Conversation>>([]);
+
+    useEffect(() => {
+        Conversations().then((conversations) => {
+            setConversations(conversations);
+        });
+    }, []);
+
+    useEffect(() => {
+        return EventsOn(`conversations-updated`, (data: any) => {
+            Conversations().then((conversations) => {
+                setConversations(conversations);
+            });
+        })
+    }, []);
+
 
     return (
         <div className="w-1/4 border-r border-gray-300 border-opacity-50 p-4 bg-gray-900">
             <h2 className="font-bold text-lg mb-4 text-gray-300">Conversations</h2>
-            {conversations.map((conversation, index) => (
-                <div
-                    key={conversation.id}
-                    className="flex items-center mb-4 cursor-pointer border-b border-gray-400 py-2"
-                >
-                    {/*<div className="w-10 h-10 rounded-full bg-gray-300 mr-2"></div>*/}
-                    <div className="flex-1 text-gray-500">
-                        <div className="flex justify-between">
-                            <p className="text-sm">Today</p>
+            <div className="h-5/6 overflow-y-auto">
+                {conversations.map((conversation, index) => (
+                    <div
+                        key={conversation.id}
+                        className="flex items-center mb-4 cursor-pointer border-b border-gray-400 py-2"
+                    >
+                        {/*<div className="w-10 h-10 rounded-full bg-gray-300 mr-2"></div>*/}
+                        <div className="flex-1 text-gray-500">
+                            <div className="flex justify-between">
+                                <p className="text-sm">Today</p>
+                            </div>
+                            <p className="text-gray-500">{conversation.title}</p>
                         </div>
-                        <p className="text-gray-500">{conversation.message}</p>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
             <SettingsButton/>
         </div>
     )
