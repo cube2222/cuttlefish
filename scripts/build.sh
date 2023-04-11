00db95d4231637c314e8c07a74bffcb4f03cc3ce
Jakub Martin <jakub.wit.martin@gmail.com>
2023-04-12 01:14:18 +0200

    Update build scripts for windows.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 wails build --noPackage --platform darwin/arm64 -o darwin/arm64/cuttlefish
 wails build --noPackage --platform darwin/amd64 -o darwin/amd64/cuttlefish
-wails build --noPackage --platform windows/arm64 -o windows/arm64/cuttlefish
-wails build --noPackage --platform windows/amd64 -o windows/amd64/cuttlefish
+wails build --noPackage --platform windows/arm64 -o windows/arm64/cuttlefish.exe
+wails build --noPackage --platform windows/amd64 -o windows/amd64/cuttlefish.exe
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,10 +8,10 @@ cd build/bin/darwin/arm64
 zip ../../../release/cuttlefish_darwin_arm64.zip cuttlefish
 cd ../../../..
 cd build/bin/windows/amd64
-zip ../../../release/cuttlefish_windows_amd64.zip cuttlefish
+zip ../../../release/cuttlefish_windows_amd64.zip cuttlefish.exe
 cd ../../../..
 cd build/bin/windows/arm64
-zip ../../../release/cuttlefish_windows_arm64.zip cuttlefish
+zip ../../../release/cuttlefish_windows_arm64.zip cuttlefish.exe
 cd ../../../..
 cd build/bin/linux/amd64
 zip ../../../release/cuttlefish_linux_amd64.zip cuttlefish
