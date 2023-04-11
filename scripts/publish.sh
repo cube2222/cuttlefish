fbd116ffef88f476002e2384f141e66ef3b1332c
Jakub Martin <jakub.wit.martin@gmail.com>
2023-04-12 01:10:16 +0200

    Update publish script.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
 mkdir build/release
-zip --junk-paths build/release/cuttlefish_darwin_amd64.zip build/bin/darwin/amd64/cuttlefish
-zip --junk-paths build/release/cuttlefish_darwin_arm64.zip build/bin/darwin/arm64/cuttlefish
-zip --junk-paths build/release/cuttlefish_windows_amd64.zip build/bin/windows/amd64/cuttlefish
-zip --junk-paths build/release/cuttlefish_windows_arm64.zip build/bin/windows/arm64/cuttlefish
-zip --junk-paths build/release/cuttlefish_linux_amd64.zip build/bin/linux/amd64/cuttlefish
+cd build/bin/darwin/amd64
+zip ../../../release/cuttlefish_darwin_amd64.zip cuttlefish
+cd ../../../..
+cd build/bin/darwin/arm64
+zip ../../../release/cuttlefish_darwin_arm64.zip cuttlefish
+cd ../../../..
+cd build/bin/windows/amd64
+zip ../../../release/cuttlefish_windows_amd64.zip cuttlefish
+cd ../../../..
+cd build/bin/windows/arm64
+zip ../../../release/cuttlefish_windows_arm64.zip cuttlefish
+cd ../../../..
+cd build/bin/linux/amd64
+zip ../../../release/cuttlefish_linux_amd64.zip cuttlefish
+cd ../../../..
 
 openssl dgst -sha256 build/release/cuttlefish_darwin_amd64.zip
 openssl dgst -sha256 build/release/cuttlefish_darwin_arm64.zip
