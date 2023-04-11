07a59654d2f6e82f0911e78e315d9e78c376b21a
Jakub Martin <jakub.wit.martin@gmail.com>
2023-04-12 00:56:00 +0200

    Update publish script.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,6 @@ zip --junk-paths build/release/cuttlefish_darwin_arm64.zip build/bin/darwin/arm6
 zip --junk-paths build/release/cuttlefish_windows_amd64.zip build/bin/windows/amd64/cuttlefish
 zip --junk-paths build/release/cuttlefish_windows_arm64.zip build/bin/windows/arm64/cuttlefish
 zip --junk-paths build/release/cuttlefish_linux_amd64.zip build/bin/linux/amd64/cuttlefish
-zip --junk-paths build/release/cuttlefish_linux_arm64.zip build/bin/linux/arm64/cuttlefish
 
 openssl dgst -sha256 build/release/cuttlefish_darwin_amd64.zip
 openssl dgst -sha256 build/release/cuttlefish_darwin_arm64.zip
